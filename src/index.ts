87b9087fbd62aef17535b61a4bf8f8f7c826aa4a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-10-01 12:48:10 +0530

    Feat: Added post route for hooks

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,43 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { StatusCodes } from 'http-status-codes';
 
 import { serverConfig, logger } from './config';
 
 const app = express();
+const client = new PrismaClient();
+
+app.post('/hooks/catch/:userId/:taskId', async (req: Request, res: Response) => {
+    try {
+        const { userId, taskId } = req.params;
+        const body = req.body;
+
+        await client.$transaction(async (tx) => {
+            const run = await tx.taskRun.create({
+                data: {
+                    taskId: taskId,
+                    metadata: body
+                }
+            });;
+
+            await tx.taskRunOutbox.create({
+                data: {
+                    taskRunId: run.id
+                }
+            });
+        });
+
+        res
+            .status(StatusCodes.CREATED)
+            .json({
+                message: "webhook recieved"
+            });
+
+        logger.info(`Webhook Received of taskId: ${taskId}`);
+    } catch (error: any) {
+        logger.error(`Error in running web hook: ${error}`);
+    }
+});
 
 app.listen(serverConfig.PORT, () => {
     logger.info(`Server Started on PORT: ${serverConfig.PORT}`);
